a5416aae224148c60f9ebcc82cf0ce57977a29a0
Frederick <11379133+FrederickRoman@users.noreply.github.com>
2021-04-11 16:25:10 -0700

    feat(MainNav): Add list items to nav drawer

--- src/components/nav/MainNav.vue
+++ src/components/nav/MainNav.vue
@@ -22,9 +22,28 @@
         <v-icon dark> mdi-format-list-bulleted-square </v-icon>
       </v-btn>
     </v-app-bar>
-    <v-navigation-drawer app v-model="drawer"
-      ><span>About</span></v-navigation-drawer
-    >
+    <v-navigation-drawer app v-model="drawer">
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="title"> Ranglish </v-list-item-title>
+          <v-list-item-subtitle> Random fake english word </v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+
+      <v-divider></v-divider>
+
+      <v-list dense nav>
+        <v-list-item v-for="item in items" :key="item.title" link>
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
   </span>
 </template>
 
@@ -37,6 +56,11 @@ export default {
   data: () => ({
     drawer: false,
     // group: null,
+    items: [
+      { title: "Settings", icon: "mdi-cog" },
+      { title: "About", icon: "mdi-help-box" },
+    ],
+    right: null,
   }),
   watch: {
     // group() {
