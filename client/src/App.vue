c427ba47edecc0e4a6c869aad50c9859f71fd951
Frederick <11379133+FrederickRoman@users.noreply.github.com>
2022-01-18 18:50:58 -0800

    refactor(App): Remove unneeded comments.

--- client/src/App.vue
+++ client/src/App.vue
@@ -11,27 +11,14 @@
 
 <script>
 import MainNav from "./components/nav/MainNav";
-// import HeroBanner from "./components/banner/HeroBanner";
-// import Home from "./pages/Home";
-
 export default {
   name: "App",
   components: { MainNav },
-  data: () => ({
-    // drawer: false,
-    // group: null,
-  }),
-  watch: {
-    // group() {
-    //   this.drawer = false;
-    // },
-  },
 };
 </script>
 
 <style lang="scss" scoped>
-@use '@/styles/colors/_colors.scss';
-
+@use "@/styles/colors/_colors.scss";
 #app-bg {
   background: colors.$background-color;
 }
