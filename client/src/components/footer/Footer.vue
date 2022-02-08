e8759cdb0ef0501520aac88bdadcb1f8bb3d25fa
Frederick <11379133+FrederickRoman@users.noreply.github.com>
2022-02-08 15:49:12 -0800

    feat(Footer): Init app Footer.

---
+++ client/src/components/footer/Footer.vue
@@ -0,0 +1,11 @@
+<template>
+  <v-footer padless>
+    <v-col class="text-center" cols="12">
+      <a href="https://frederickroman.com" target="_blank" rel="noopener"
+        ><strong>&copy; Frederick Roman</strong>
+        {{ new Date().getFullYear() }}</a
+      >
+    </v-col>
+  </v-footer>
+</template>
+
