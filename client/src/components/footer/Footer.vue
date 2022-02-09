1ee1f3d978b361bd3ea84953a7de7e97975c5b2c
Frederick <11379133+FrederickRoman@users.noreply.github.com>
2022-02-08 16:49:12 -0800

    feat(Footer): Add ExternalLink component.

--- client/src/components/footer/Footer.vue
+++ client/src/components/footer/Footer.vue
@@ -1,11 +1,24 @@
 <template>
   <v-footer padless>
     <v-col class="text-center" cols="12">
-      <a href="https://frederickroman.com" target="_blank" rel="noopener"
-        ><strong>&copy; Frederick Roman</strong>
-        {{ new Date().getFullYear() }}</a
-      >
+      <ExternalLink href="https://frederickroman.com">
+        <strong> &copy; Frederick Roman </strong> {{ currentYear }}
+      </ExternalLink>
     </v-col>
   </v-footer>
 </template>
+
+<script>
+import ExternalLink from "@/components/link/ExternalLink";
+
+const DEFAULT_YEAR = 2022;
+export default {
+  name: "Footer",
+  components: { ExternalLink },
+  data: () => ({ currentYear: DEFAULT_YEAR }),
+  created() {
+    this.currentYear = new Date().getFullYear();
+  },
+};
+</script>
 
