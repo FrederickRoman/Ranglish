5606f10b54736e77ba3173168cbe19123f27acf7
Frederick <11379133+FrederickRoman@users.noreply.github.com>
2022-02-08 16:48:41 -0800

    feat(link): Create ExternalLink component.

---
+++ client/src/components/link/ExternalLink.vue
@@ -0,0 +1,17 @@
+<template>
+  <a :href="href" target="_blank" rel="noopener" id="external-link_anchor">
+    <slot />
+  </a>
+</template>
+
+<script>
+export default {
+  name: "ExternalLink",
+  props: {
+    href: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
