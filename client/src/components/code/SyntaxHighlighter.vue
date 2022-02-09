8281cae7042bdc87c24ab62605c04191eecea0a8
Frederick <11379133+FrederickRoman@users.noreply.github.com>
2022-02-09 12:01:43 -0800

    feat(SyntaxHighlighter): Init syntax highlighter code component.

---
+++ client/src/components/code/SyntaxHighlighter.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-card>
+    <pre class="language-js"><code>{{API_CALL_JS_CODE}}</code></pre>
+  </v-card>
+</template>
+
+<script>
+import Prism from "prismjs";
+
+const API_CALL_JS_CODE =
+  `fetch('https://ranglish.herokuapp.com/api/word')` +
+  "\n" +
+  ".then(response => response.json())" +
+  "\n" +
+  ".then(json => console.log(json))";
+
+export default {
+  name: "SyntaxHighlighter",
+  data: () => ({ API_CALL_JS_CODE }),
+  mounted() {
+    Prism.highlightAll();
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import "~prismjs/themes/prism-okaidia.css";
+</style>
+
