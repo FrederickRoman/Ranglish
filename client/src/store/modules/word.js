5a01843b72c046795b351620010cf950da93de17
Frederick <11379133+FrederickRoman@users.noreply.github.com>
2022-02-09 16:09:20 -0800

    refactor(store): Remove unneeded comments and logs.

--- client/src/store/modules/word.js
+++ client/src/store/modules/word.js
@@ -1,6 +1,5 @@
 import axios from "axios";
 
-// import { WORD_STATE } from "@/store/state-types.js";
 import { SET_WORD } from "@/store/mutation-types.js";
 import { FETCH_WORD } from "@/store/action-types.js";
 
@@ -27,7 +26,6 @@ export default {
         const URL = "api/word";
         const res = await axios.get(URL);
         const word = await res.data;
-        console.log("word", word);
         commit(SET_WORD, word);
       } catch (error) {
         console.log(error);
